93e55d502c2dab5f0e16fd7db1cb7f5d6bfe1a91
jasonOu <1@2.com>
2020-12-08 11:22:57 +0800

    Initialize AD network SDK after App is launched.

--- CatAndDog/AppDelegate.swift
+++ CatAndDog/AppDelegate.swift
@@ -8,12 +8,16 @@
 
 import UIKit
 import CoreData
+import GoogleMobileAds
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        // Initialize Google Mobile Ads SDK
+        GADMobileAds.sharedInstance().start(completionHandler: nil)
+        
         return true
     }
     
