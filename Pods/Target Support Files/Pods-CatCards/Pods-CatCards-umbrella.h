8ec9216bc80773a7de8f1ea48f99876c646fcd0e
jasonOu <1@2.com>
2021-01-07 17:39:06 +0800

    Rename all project related files to CatCards.

--- Pods/Target Support Files/Pods-CatCards/Pods-CatCards-umbrella.h
+++ Pods/Target Support Files/Pods-CatCards/Pods-CatCards-umbrella.h
@@ -11,6 +11,6 @@
 #endif
 
 
-FOUNDATION_EXPORT double Pods_CatAndDogVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_CatAndDogVersionString[];
+FOUNDATION_EXPORT double Pods_CatCardsVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_CatCardsVersionString[];
 
