a3f427048823444b66d6490f0b91dba8806728c9
jasonOu <1@2.com>
2021-03-24 16:00:43 +0800

    Complete UI tests by validating every view controller and its UI components.

--- Cat Cards UITests/Cat_Cards_UITests.swift
+++ Cat Cards UITests/Cat_Cards_UITests.swift
@@ -9,36 +9,139 @@
 import XCTest
 
 class Cat_Cards_UITests: XCTestCase {
-
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-
+    
+    private let numberOfSavedImages = 5
+    
+    override func setUp() {
+        super.setUp()
         // In UI tests it is usually best to stop immediately when a failure occurs.
         continueAfterFailure = false
-
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
     }
 
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
+    func testHomeVC() {
         // UI tests must launch the application that they test.
         let app = XCUIApplication()
         app.launch()
 
         // Use recording to get started writing UI tests.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
-        app.buttons["shareButton"].tap()
+        
+        let currentCard = app.children(matching: .window).element(boundBy: 0).children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.children(matching: .other).element(boundBy: 0).children(matching: .other).element(boundBy: 0).children(matching: .image).element(boundBy: 1)
+        
+        currentCard.tap()
+        sleep(1)
+        currentCard.tap()
+        sleep(1)
+        
+        let swipeTime = numberOfSavedImages
+        let toolbar = app.toolbars["Toolbar"]
+        let saveButton = toolbar/*@START_MENU_TOKEN@*/.buttons["saveButton"]/*[[".buttons[\"Save\"]",".buttons[\"saveButton\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/
+        let undoButton = toolbar/*@START_MENU_TOKEN@*/.buttons["undoButton"]/*[[".buttons[\"Undo\"]",".buttons[\"undoButton\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/
+        let shareButton = toolbar/*@START_MENU_TOKEN@*/.buttons["shareButton"]/*[[".buttons[\"Share\"]",".buttons[\"shareButton\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/
+        
+        for _ in 0..<swipeTime {
+            currentCard.swipeToRandomSide(velocity: 150)
+            sleep(1)
+            saveButton.tap()
+        }
+        
+        
+        for _ in 0..<swipeTime {
+            undoButton.tap()
+            sleep(1)
+        }
+        
+        
+        shareButton.tap()
+        app/*@START_MENU_TOKEN@*/.navigationBars["UIActivityContentView"]/*[[".otherElements[\"ActivityListView\"].navigationBars[\"UIActivityContentView\"]",".navigationBars[\"UIActivityContentView\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.buttons["Close"].tap()
+        
+        toolbar/*@START_MENU_TOKEN@*/.buttons["saveButton"]/*[[".buttons[\"Save\"]",".buttons[\"saveButton\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
+        sleep(1)
+    }
+    
+    func testSegue() {
+        let app = XCUIApplication()
+        app.activate()
+        sleep(5)
+        
+        app.toolbars["Toolbar"]/*@START_MENU_TOKEN@*/.buttons["saveButton"]/*[[".buttons[\"Save\"]",".buttons[\"saveButton\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
+        sleep(1)
+        
+        if app.alerts.count != 0 {
+            app.alerts["Storage is full."].scrollViews.otherElements.buttons["OK"].tap()
+            sleep(1)
+        }
+        
+        
+        app.navigationBars["Cat_Cards.HomeVC"]/*@START_MENU_TOKEN@*/.buttons["collectionButton"]/*[[".buttons[\"grid 2x2\"]",".buttons[\"collectionButton\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
+        sleep(1)
+        app.collectionViews.cells.images.firstMatch.tap()
+        sleep(1)
+        app.navigationBars["Cat_Cards.SingleImageVC"].buttons["Back"].tap()
+        sleep(1)
+        app.navigationBars["Cat_Cards.CollectionVC"].buttons["Back"].tap()
+        sleep(1)
+        
+    }
+    
+    func testSingleImageVC() {
+        let app = XCUIApplication()
+        app.activate()
+        
+        // Go to SingleImageVC.
+        app.navigationBars["Cat_Cards.HomeVC"]/*@START_MENU_TOKEN@*/.buttons["collectionButton"]/*[[".buttons[\"grid 2x2\"]",".buttons[\"collectionButton\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
+        app.collectionViews.cells.images.firstMatch.tap()
+        
+        // Test tap and pinch gesture recognizers.
+        let firstImage = app.scrollViews.images.firstMatch
+        firstImage.doubleTap()
+        firstImage.pinch(withScale: 2.0, velocity: 30)
+        firstImage.doubleTap()
+        
+        // Test scrollView
+        for i in 0..<numberOfSavedImages {
+            app.scrollViews.images.allElementsBoundByIndex[i].swipeLeft()
+        }
+        
+        for j in 0..<numberOfSavedImages {
+            app.scrollViews.images.allElementsBoundByIndex[numberOfSavedImages - j].swipeRight()
+        }
+        
+        // Test share sheet and image deletion.
+        let toolbar = app.toolbars["Toolbar"]
+        toolbar.buttons["Share"].tap()
+        app/*@START_MENU_TOKEN@*/.navigationBars["UIActivityContentView"]/*[[".otherElements[\"ActivityListView\"].navigationBars[\"UIActivityContentView\"]",".navigationBars[\"UIActivityContentView\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.buttons["Close"].tap()
+        toolbar.buttons["Delete"].tap()
+        app.sheets["This action can not be reverted."].scrollViews.otherElements.buttons["Delete Picture"].tap()
+        
     }
 
-//    func testLaunchPerformance() throws {
-//        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, *) {
-//            // This measures how long it takes to launch your application.
-//            measure(metrics: [XCTApplicationLaunchMetric()]) {
-//                XCUIApplication().launch()
-//            }
-//        }
-//    }
+    func testLaunchPerformance() throws {
+        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, *) {
+            // This measures how long it takes to launch your application.
+            measure(metrics: [XCTApplicationLaunchMetric()]) {
+                XCUIApplication().launch()
+            }
+        }
+    }
+}
+
+extension XCUIElement {
+    enum SwipeSide: CaseIterable {
+        case right, left, up, down
+    }
+    
+    func swipeToRandomSide(velocity: XCUIGestureVelocity) {
+        let randomSide = SwipeSide.allCases.randomElement()!
+        switch randomSide {
+        case .right:
+            self.swipeRight(velocity: velocity)
+        case .left:
+            self.swipeLeft(velocity: velocity)
+        case .up:
+            self.swipeUp(velocity: velocity)
+        case .down:
+            self.swipeDown(velocity: velocity)
+        }
+    }
 }
